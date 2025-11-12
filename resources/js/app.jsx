f0b2226e93a03af7197400303332acf01606f1f9
Nguyntanh <nguyntanh2k5@gmail.com>
2025-11-12 11:42:48 +0700

    hide report page

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import DashboardLayout from "./layouts/DashboardLayout";
 import Dashboard from "./components/Dashboard";
 import UsersPage from "./pages/UsersPage";
-import ReportPage from "./pages/ReportPage";
+// import ReportPage from "./pages/ReportPage";
 import CyclesPanel from "./pages/CyclesPanel";
 import DepartmentsPanel from "./pages/DepartmentsPanel";
 import ObjectivesPage from "./pages/ObjectivesPage";
@@ -369,11 +369,9 @@ export default function App() {
         else if (p.startsWith("/profile")) content = <ProfilePage />;
         else if (p.startsWith("/change-password"))
             content = <ChangePasswordPage />;
-        else if (p.startsWith("/reports")) content = <ReportPage />;
-        else if (p.startsWith("/dashboard"))
-            content = <Dashboard />;
-        else if (p.startsWith("/reports"))
-            content = <CompanyOverviewReport />;
+        // else if (p.startsWith("/reports")) content = <ReportPage />;
+        else if (p.startsWith("/dashboard")) content = <Dashboard />;
+        else if (p.startsWith("/reports")) content = <CompanyOverviewReport />;
         return <DashboardLayout user={user}>{content}</DashboardLayout>;
     }
 
